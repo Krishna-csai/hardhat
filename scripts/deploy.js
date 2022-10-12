ab93e28103ad21ea90268d0c4b10aaf8dce46e66
Krishna-csai <krishna2021csai133@abesit.edu.in>
2022-10-12 23:05:22 +0530

    fg

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,11 +4,11 @@ const { ethers, run } = require("hardhat");
 async function main() {
   const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
   console.log("Deploying Contract ...");
-  const SimpleStorage = await SimpleStorageFactory.deploy();
-  await SimpleStorage.deployed();
-  const store = await SimpleStorage.store(4);
-  const FavouriteNumber = await SimpleStorage.retreive();
-  console.log(`Deploying Contract to ${SimpleStorage.address}`);
+  const fuck = await SimpleStorageFactory.deploy();
+  await fuck.deployed();
+  const store = await fuck.store(4);
+  const FavouriteNumber = await fuck.retreive();
+  console.log(`Deploying Contract to ${fuck.address}`);
   console.log(FavouriteNumber);
   // verify(SimpleStorage.address, []);
 }
